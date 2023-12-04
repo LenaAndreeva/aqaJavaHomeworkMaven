74db0c95e423b193a0cf89650770476361628084
Lena Andreeva <lena.an6565@gmail.com>
2023-12-04 23:02:31 +0300

    1

--- src/test/java/org/andreevaelena/homework/AppTest.java
+++ src/test/java/org/andreevaelena/homework/AppTest.java
@@ -1,9 +1,8 @@
 package org.andreevaelena.homework;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-/**
- * Unit test for simple App.
- */
+
 public class AppTest {
     @Test
     public void testApp() {
