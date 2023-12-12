b84420019dac13a96cbb660a83a5ed14933a25bd
Lena Andreeva <lena.an6565@gmail.com>
2023-12-13 00:02:41 +0300

    1

--- src/test/java/org/andreevaelena/homework/TestToCompareTheNumberOfProducts.java
+++ src/test/java/org/andreevaelena/homework/TestToCompareTheNumberOfProducts.java
@@ -19,10 +19,6 @@ public class TestToCompareTheNumberOfProducts extends BaseTest {
                 .goToCart();
         cartPage
                 .compareNumberOfProducts();
-        v
-
-
-
 
     }
 }
